7fa0634cd6bc6f0074fe6fb424d8aed6d7a2374e
LukeLarson2 <lucas.m.larson2@gmail.com>
2023-12-23 18:24:45 -0800

    added deliverables widget and styling

--- src/app/project/Deliverables.jsx
+++ src/app/project/Deliverables.jsx
@@ -1,22 +1,76 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import formatDate from "../_utils/formatDate.js";
+import truncateString from "../_utils/truncateString.js";
+import getUserColor from "../_utils/getUserColor.js";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import TaskModal from "./TaskModal.jsx";
 
-export default function Deliverables({ deliverables }) {
+export default function Deliverables({
+  deliverables,
+  handleMarkComplete,
+  handleClaimTask,
+  project_meta,
+}) {
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = (value) => {
+    setShowModal(value);
+  };
+
+  const handleSelectTask = (task, date) => {
+    setSelectedTask(task);
+    setSelectedDate(date);
+    handleShowModal(true);
+  };
+
   return (
-    <div>
+    <div className="deliverables-main">
+      {showModal && (
+        <TaskModal
+          task={selectedTask}
+          handleShowModal={handleShowModal}
+          handleMarkComplete={handleMarkComplete}
+          handleClaimTask={handleClaimTask}
+          project_meta={project_meta}
+          date={selectedDate}
+        />
+      )}
       {deliverables.map((day) => {
-        <div className="deliverables-day">
-          {day.tasks.map((task) => {
-            return (
-              <div key={task.task_id}>
-                <p>{task.date}</p>
-                <p></p>
-              </div>
-            );
-          })}
-        </div>;
+        const date = formatDate(day.date);
+        return (
+          <div className="deliverables-day" key={day.date}>
+            <h3>{date}</h3>
+            {day.tasks.map((task) => {
+              const title = truncateString(task.title, 10);
+              const userIndex = project_meta.team.indexOf(task.owner);
+              const color = getUserColor(userIndex);
+              return (
+                <div
+                  key={task.task_id}
+                  className="day-task-main"
+                  onClick={() => handleSelectTask(task, date)}
+                  style={{
+                    justifyContent: task.owner ? "flex-start" : "center",
+                  }}
+                >
+                  {task.owner && (
+                    <div className={`task-owner-name-${color}`}>
+                      @{task.owner}
+                    </div>
+                  )}
+                  <p className="day-task-title">{title}</p>
+                </div>
+              );
+            })}
+          </div>
+        );
       })}
+      <FaChevronLeft className="deliverables-left-arrow" />
+      <FaChevronRight className="deliverables-right-arrow" />
     </div>
   );
 }
