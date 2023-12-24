4ba37a0acc029574693ed3e3c3c2bec60dac3417
LukeLarson2 <lucas.m.larson2@gmail.com>
2023-12-23 18:26:42 -0800

    added link to the project-meta repo page

--- src/app/project/page.js
+++ src/app/project/page.js
@@ -8,14 +8,71 @@ import ProjectDetails from "./ProjectDetails.jsx";
 
 import "./style.css";
 
-const { project_meta, deliverables, messages } = mockData;
-
 export default function ProjectPage() {
+  const [project_meta, setProject_meta] = useState(null);
+  const [deliverables, setDeliverables] = useState(null);
+  const [messages, setMessages] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [triggerUpdate, setTriggerUpdate] = useState(false);
+  const handleMarkComplete = (task_id) => {
+    deliverables.forEach((deliverable) => {
+      deliverable.tasks.forEach((task) => {
+        if (task.task_id === task_id) {
+          const toggleValue = task.complete === false ? true : false;
+          task.complete = toggleValue;
+        }
+      });
+    });
+    setTriggerUpdate(!triggerUpdate);
+  };
+
+  const getData = async () => {
+    setDeliverables(mockData.deliverables);
+    setMessages(mockData.messages);
+    setProject_meta(mockData.project_meta);
+  };
+
+  const handleClaimTask = async (username, task_id) => {
+    deliverables.forEach((deliverable) => {
+      deliverable.tasks.forEach((task) => {
+        if (task.task_id === task_id) {
+          task.owner = username;
+        }
+      });
+    });
+    setTriggerUpdate(!triggerUpdate);
+  };
+
+  useEffect(() => {
+    getData();
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    setDeliverables(mockData.deliverables);
+  }, [triggerUpdate]);
+
+  if (isLoading) {
+    return <span className="loader"></span>;
+  }
+
   return (
     <div className="main-container">
       <div className="project-deliverables-link">
         <ProjectDetails project_meta={project_meta} />
-        <Deliverables deliverables={deliverables} />
+        <Deliverables
+          deliverables={deliverables}
+          handleMarkComplete={handleMarkComplete}
+          handleClaimTask={handleClaimTask}
+          project_meta={project_meta}
+        />
+        <a
+          href={project_meta.repo_link}
+          className="link-project"
+          target="_blank"
+        >
+          LINK TO PROJECT REPO
+        </a>
       </div>
       <MessageBoard messages={messages} project_meta={project_meta} />
     </div>
