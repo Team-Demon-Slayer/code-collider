7ed622d9e9800ca79a89ddd0c80f825ca6be1ba6
Xianyi Liang <liangxianyi2011@gmail.com>
2024-01-02 15:21:43 -0800

    Add userdata to show case page as well

--- src/app/community/showcase/[page]/page.js
+++ src/app/community/showcase/[page]/page.js
@@ -1,21 +1,33 @@
 'use client';
 
+import { useEffect } from 'react';
 import useCommunityContext from '../../useCommunityContext';
 import { projects, query } from '../../temp-fake-data';
 import ProjectCard from '../../ProjectCard';
 
 export default function ShowcasePage({ params: { page } }) {
-  const { keyword, language, spots, startDate, openMentor } =
-    useCommunityContext();
+  const {
+    keyword,
+    language,
+    spots,
+    startDate,
+    openMentor,
+    user,
+    storeUserDataFromSupabase
+  } = useCommunityContext();
+  useEffect(() => {
+    if (!user) {
+      storeUserDataFromSupabase();
+    }
+  }, [user, storeUserDataFromSupabase]);
   return (
     <div className="page-container">
       <div>
-        {query(projects, keyword, language, spots, openMentor).map(
-          p => (
-            <ProjectCard key={p.title} project={p} />
-          )
-        )}
+        {query(projects, keyword, language, spots, openMentor).map(p => (
+          <ProjectCard key={p.title} project={p} />
+        ))}
       </div>
+      {JSON.stringify(user)}
     </div>
   );
 }
