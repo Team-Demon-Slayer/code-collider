f02f1da621eb98f7af5e25da471a663284416f58
Gordon <gpchiu1213@gmail.com>
2024-01-02 14:12:19 -0800

    Created Pages and Planned Files

---
+++ src/app/my-account/page.js
@@ -0,0 +1,15 @@
+"use client";
+import "./styles.css";
+import Profile from "./Profile.jsx";
+export default function Account() {
+  return <main>
+    <div className="main-container">Hello
+      <div className="column1">
+        {/* <Profile /> */}
+      </div>
+      <div className="column2">
+
+      </div>
+    </div>
+  </main>;
+}
