a3f1d8929d825b584ac8fd47dee7dd47ed573902
Gordon <gpchiu1213@gmail.com>
2024-01-02 17:09:46 -0800

    Created Template Layout of my-account page

---
+++ src/app/my-account/_components/Experience.jsx
@@ -0,0 +1,11 @@
+"use client";
+import "../styles.css";
+export default function Experience() {
+  return <main>
+    <div className= "experience-info">
+      <div className="experience-header">
+        Experience Level
+      </div>
+    </div>
+  </main>;
+}
---
+++ src/app/my-account/_components/Languages.jsx
@@ -0,0 +1,11 @@
+"use client";
+import "../styles.css";
+export default function Languages() {
+  return <main>
+    <div className= "languages-info">
+      <div className="languages-header">
+        Languages
+      </div>
+    </div>
+  </main>;
+}
