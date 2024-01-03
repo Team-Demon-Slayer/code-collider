2fd289f3048440f48e28e19906ba98cc95d4f4f0
PJ O'Leary <peej@oleary.com>
2024-01-03 13:46:15 -0800

    Added My Projects Queries

--- src/app/api/page.js
+++ src/app/api/page.js
@@ -2,18 +2,15 @@
 import { useState, useEffect } from 'react';
 import { getDeliverables } from './_db/_models/deliverablesModels.js';
 import { getExpandedUser } from './_db/_models/usersModels.js';
-import { getProjectPage } from './_db/_models/projectsModels.js';
+import { getProjectPage, joinProject, getMyProjects, getMyMentorProjects } from './_db/_models/projectsModels.js';
 
 export default function Api() {
-  const [deliverables, setDeliverables] = useState({});
-  const [user, setUser] = useState({});
-  const [projects, setProjects] = useState({});
-
 
   useEffect(() => {
     getDeliverables('512b933e-aa62-11ee-b32a-02c6baa627c7')
     .then((res) => {
-      setDeliverables(res);
+      console.log('Get Deliverables');
+      console.log(res);
     })
     .catch((err) => {
       console.error(err);
@@ -21,22 +18,49 @@ export default function Api() {
 
     getExpandedUser('9c202e1c-2ad3-403e-b0ee-62310138aa88')
     .then((res) => {
+      console.log('Get User');
       console.log(res);
-      setUser(res);
     })
     .catch((err) => {
-      console.log(res);
       console.error(err);
     });
 
     getProjectPage()
     .then((res) => {
+      console.log('Project Page');
       console.log(res);
-      setProjects(res);
     })
     .catch((err) => {
       console.error(err);
     });
+
+    joinProject('512b933e-aa62-11ee-b32a-02c6baa627c7')
+    .then((res) => {
+      console.log('Join Project');
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+    getMyProjects('9c202e1c-2ad3-403e-b0ee-62310138aa88')
+    .then((res) => {
+      console.log('Get My Projects');
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+    getMyMentorProjects('9c202e1c-2ad3-403e-b0ee-62310138aa88')
+    .then((res) => {
+      console.log('Get My Mentor Projects');
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
   }, [])
 
   const divStyle = {
@@ -45,9 +69,6 @@ export default function Api() {
 
   return (
     <div>
-      <div style={divStyle}>{JSON.stringify(deliverables)}</div>
-      <div style={divStyle}>{JSON.stringify(user)}</div>
-      <div style={divStyle}>{JSON.stringify(projects)}</div>
     </div>
   );
 }
