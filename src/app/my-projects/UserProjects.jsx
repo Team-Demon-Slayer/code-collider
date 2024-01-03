dd69582340c8522c5df88745cd8d8b17b3599d44
Faycal <rwigemafayk@gmail.com>
2024-01-03 14:48:11 -0800

    myProjects fix

--- src/app/my-projects/UserProjects.jsx
+++ src/app/my-projects/UserProjects.jsx
@@ -5,54 +5,34 @@ import { useRouter } from "next/navigation";
 import Carousel from "../_components/Carousel.jsx";
 import "./myProjects.css";
 
-export default function UserProjects({ data }) {
+export default function UserProjects({ currentProjects, pastProjects }) {
   const router = useRouter();
-  
-  const [currentProjects, setCurrentProjects] = useState(null);
-  const [pastProjects, setSpotPastProjects] = useState(null);
+
   const [navToSelected, setNavToSelected] = useState(null);
 
-  const getCurrentProjects = () => {
-    const current = data.filter((project) => {
-      return project.active;
-    });
-    setCurrentProjects(current);
-  };
-
-  const getPastProjects = () => {
-    const past = data.filter((project) => {
-      return !project.active;
-    });
-    setSpotPastProjects(past);
-  };
-
   const navToProject = (obj) => {
-    router.push(`/project/${obj.project_id}`);
+    router.push(`/project/${obj.id}`);
   };
-
-  useEffect(() => {
-    getCurrentProjects();
-    getPastProjects();
-  }, [data]);
 
   return (
-    currentProjects &&
-    pastProjects && (
-      <div className="my-projects-container">
-        {/* <Carousel
+    <div className="my-projects-container">
+      {currentProjects && (
+        <Carousel
           projects={currentProjects}
           getProject={navToProject}
           header="Current Projects"
-        /> */}
+        />
+      )}
 
-        <div id="my-projects-split" />
-{/*
+      <div id="my-projects-split" />
+
+      {pastProjects && (
         <Carousel
           projects={pastProjects}
           getProject={navToProject}
           header="Past Projects"
-        /> */}
-      </div>
-    )
+        />
+      )}
+    </div>
   );
 }
