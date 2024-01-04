d67b74f0ea4e988be3c6dca45890405c5b24b2e0
Gordon <gpchiu1213@gmail.com>
2024-01-03 16:16:15 -0800

    Style Profile, Bio, and Experience Content

--- src/app/my-account/page.js
+++ src/app/my-account/page.js
@@ -1,22 +1,42 @@
 "use client";
 import "./styles.css";
+// import {createClientComponentClient} from "@supabase/supabase-js";
+// import {v4 as uuidv4} from "uuid";
+// import { supabase } from "../../supabaseClient.js";
+import React, { useState, useEffect } from "react";
 import Profile from "./_components/Profile.jsx";
 import Languages from "./_components/Languages.jsx";
 import Experience from "./_components/Experience.jsx";
 import Bio from "./_components/Bio.jsx";
+
 export default function Account() {
-  return <main>
+  const [user, setUser] = useState(null);
+  const userData = {
+    username: "John Doe",
+    experience: "Intermediate",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu ac tortor dignissim convallis aenean et tortor. Ut diam quam nulla porttitor massa id. Sed blandit libero volutpat sed cras ornare arcu. Nisi porta lorem mollis aliquam ut porttitor leo a diam. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Justo eget magna fermentum iaculis eu non diam phasellus vestibulum. Ipsum consequat nisl vel pretium lectus quam id leo. At risus viverra adipiscing at in tellus integer feugiat.",
+    profile_photo: "https://i.imgur.com/6VBx3io.jpg",
+    is_mentor: true,
+  };
+  const handleUser = (user) => {
+    setUser(user);
+  };
+  useEffect(() => {
+    setUser(userData);
+      }, []);
+  return user ? (
+  <main className="account-page">
     <div className="main-container">
       <div className="column1">
-        <Profile />
+        <Profile user= {user}/>
       </div>
       <div className="column2">
-        <Languages />
-        <Experience />
+        <Languages user= {user}/>
+        <Experience user= {user}/>
       </div>
     </div>
     <div className= "bio-container">
-      <Bio/>
+      <Bio user= {user}/>
     </div>
-  </main>;
+  </main>) : <div>loading</div>;
 }
