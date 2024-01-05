702d412c3c697c8846739ca78c252eb9fd6c0d0e
Xianyi Liang <liangxianyi2011@gmail.com>
2024-01-04 21:45:40 -0800

    search function

--- src/app/community/CommunityContext.jsx
+++ src/app/community/CommunityContext.jsx
@@ -10,11 +10,13 @@ export function CommunityProvider({ children }) {
   const [keyword, setKeyword] = useState('');
   const [language, setLanguage] = useState('');
   const [spots, setSpots] = useState('');
-  const [startDate, setStartDate] = useState(new Date('2021-01-01'));
+  const [startDate, setStartDate] = useState('');
   const [openMentor, setOpenMentor] = useState(true);
   const [user, setUser] = useState(null);
   const [languageSelected, setLanguageSelected] = useState(true);
+  const [selectedLanguage, setSelectedLanguage] = useState('');
   const debouncedKeyword = useDebounce(keyword, 1000);
+
 
   useEffect(() => {
     async function getUser() {
@@ -39,9 +41,22 @@ export function CommunityProvider({ children }) {
       user,
       languageSelected,
       setLanguageSelected,
-      debouncedKeyword
+      debouncedKeyword,
+      selectedLanguage,
+      setSelectedLanguage
     }),
-    [keyword, language, spots, startDate, openMentor, user, languageSelected, debouncedKeyword]
+    [
+      keyword,
+      language,
+      spots,
+      startDate,
+      openMentor,
+      user,
+      languageSelected,
+      debouncedKeyword,
+      selectedLanguage,
+      setSelectedLanguage
+    ]
   );
 
   return (
