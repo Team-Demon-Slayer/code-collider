50e1e630f221c797805f943f69e96763751412bf
Colin Cooley <colincooley1989@gmail.com>
2024-01-04 15:23:21 -0900

    fixed redirect link

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import "./Login.css";
 import Login from "./Login";
-import RootLayout from "../../../app/layout";
 
 export default function LoginPage() {
   return (
