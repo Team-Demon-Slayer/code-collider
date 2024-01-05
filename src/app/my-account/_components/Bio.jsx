6ef7dc2ee0dea207109f77fa4071f9c24c9dbeae
Gordon <gpchiu1213@gmail.com>
2024-01-04 16:22:41 -0800

    Changed Bio Edit from Modal to Inline Styling, Completed Profile Functionality

--- src/app/my-account/_components/Bio.jsx
+++ src/app/my-account/_components/Bio.jsx
@@ -2,8 +2,8 @@
 import "../page.css";
 import React, { useState, useEffect } from "react";
 import supabase from "../../api/_db/index.js";
-import BioModal from "./BioModal.jsx";
 import { FaPencil } from "react-icons/fa6";
+import { AiOutlineClose } from 'react-icons/ai';
 export default function Bio({user,handleUpdate}) {
   const [editState, setEditState] = useState(false);
   const [text, setText] = useState(user.bio);
@@ -19,7 +19,6 @@ export default function Bio({user,handleUpdate}) {
     }
   }
   const handleUpdateBio = async () => {
-    console.log(text);
     const { data, error } = await supabase
       .from('users')
       .update({
@@ -34,14 +33,16 @@ export default function Bio({user,handleUpdate}) {
     setChanged(false);
     setEditState(!editState);
   };
-  return
+  return(
     <div className= "bio-info">
       <div className="bio-header" onClick={handleEdit}>
         Bio &nbsp;
-        <FaPencil className="edit-icon-bio"/>
+        {!editState ? <FaPencil className="edit-icon-bio"/>:<AiOutlineClose className="bio-cancel"/>}
       </div>
       <div className="bio-content">
-      <textarea className={!editState ? "bio-textarea":"bio-textarea-edit"} placeholder="Write a short bio about yourself..." onChange={(e)=>handleTextChange(e)} disabled={!editState}>{user.bio}</textarea>
-       { changed && <button className="save-button" onClick={handleUpdateBio}>Save</button> }
-  ;
+      <textarea className={!editState ? "bio-textarea":"bio-textarea-edit"} defaultValue={user.bio} placeholder="Write a short bio about yourself..." onChange={(e)=>handleTextChange(e)} disabled={!editState}></textarea>
+       { changed && <button className="save-button" onClick={handleUpdateBio}>Save</button>};
+      </div>
+    </div>
+  )
 }
--- src/app/my-account/_components/ProfileModal.jsx
+++ src/app/my-account/_components/ProfileModal.jsx
@@ -3,12 +3,44 @@ import "../modals.css";
 import supabase from '../../api/_db/index.js';
 import { AiOutlineClose } from 'react-icons/ai';
 import React, { useState, useEffect } from "react";
-export default function ProfileModal({user, handleModal}) {
+export default function ProfileModal({user, handleModal, avatarList, handleUpdate}) {
+  const [avatar, setAvatar] = useState(user.profile_photo);
+  const [changed, setChanged] = useState(false);
+  const handleAvatarChange = (e) => {
+    setAvatar(e.target.src);
+    if (e.target.src === user.profile_photo) {
+      setChanged(false);
+    }
+    else (
+      setChanged(true)
+    )
+    console.log(user.profile_photo);
+  }
+  const handleUpdateAvatar = async () => {
+    const { data, error } = await supabase
+      .from('users')
+      .update({
+        profile_photo: avatar,
+      })
+      .eq('id', user.id)
+    handleUpdate();
+    handleModal();
+  }
   return <main>
   <div className= "overlay">
     <div className="modal">
-      ProfileModal
+      <div className="modal-header">Select a Profile Photo</div>
+      <div className="avatar-list">
+      {
+        avatarList.map((item, index) => {
+          return <img key={index} src={item} onClick={handleAvatarChange} alt="avatar" value={item} className={(avatar===item) ? "avatar-selected":"avatar"}/>;
+        })
+      }
+      </div>
+      { changed&& <button className="save-button" onClick={handleUpdateAvatar}>Save</button> }
+        <AiOutlineClose className="close-modal" onClick={handleModal}/>
     </div>
+
   </div>
 </main>;
 }
